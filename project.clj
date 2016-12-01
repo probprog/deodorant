508b985513e62c6f6abdbea052b94405ee7e63cf
Tuan-Anh Le <tuananh@robots.ox.ac.uk>
2016-12-01 20:15:54 +0000

    remove anglican deps

--- project.clj
+++ project.clj
@@ -4,5 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clatrix "0.5.0"]
-                 [anglican "1.1.0-SNAPSHOT-BOPP"]])
+                 [clatrix "0.5.0"]])
